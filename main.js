224c5c9bb288a48c7052f6447e81e94b349b1937
Jomar Rumbawa <hello@jomarrumbawa.com>
2025-10-16 11:53:12 -0700

    feat: add playRound function and logic

--- main.js
+++ main.js
@@ -11,3 +11,33 @@ function getHumanChoice() {
   const choice = prompt("Rock, paper or scissors?")
   return choice.toLowerCase()
 }
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice == 'rock' && computerChoice == 'scissors') {
+    humanScore++
+    console.log("You win! Rock beats scissors.")
+  } else if (humanChoice == 'scissors' && computerChoice == 'paper') {
+    humanScore++
+    console.log("You win! Scissors beats paper.")
+  } else if (humanChoice == 'paper' && computerChoice == 'rock') {
+    humanScore++
+    console.log("You win! Paper beats rock")
+  } else if (humanChoice == 'rock' && computerChoice == 'paper') {
+    computerScore++
+    console.log("You lose! Paper beats rock.")
+  } else if (humanChoice == 'paper' && computerChoice == 'scissors') {
+    computerScore++
+    console.log("You lose! Scissors beats paper.")
+  } else if (humanChoice == 'scissors' && computerChoice == 'rock') {
+    computerScore++
+    console.log("You lose! Rock beats scissors.")
+  } else {
+    console.log ("It's a tie.")
+  }
+}
+
+playRound(getHumanChoice(), getComputerChoice())
+playRound(getHumanChoice(), getComputerChoice())
+playRound(getHumanChoice(), getComputerChoice())
+playRound(getHumanChoice(), getComputerChoice())
+playRound(getHumanChoice(), getComputerChoice())
